3efe5494ab4b437b8115799532a7999fce04ccbd
Denis <80954428+DeniskoID@users.noreply.github.com>
2023-06-19 19:29:32 +0300

    Configures es-lint & style-lint & prettier. Creates some components new file:   .editorconfig new file:   .eslintignore new file:   .eslintrc new file:   .prettierignore new file:   .prettierrc new file:   .stylelintrc.json new file:   src/components/Categories.jsx new file:   src/components/Header.jsx new file:   src/components/PizzaBlock.jsx new file:   src/components/Sort.jsx

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
