46e0442de34a0023b1cc523faf5ad654b781f2f2
Denis <80954428+DeniskoID@users.noreply.github.com>
2023-07-10 21:58:07 +0300

    Creates an interaction for Cart component new file:   src/components/CartEmpty.jsx new file:   src/components/CartItem.jsx new file:   src/redux/slices/cartSlice.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import logoSvg from '../assets/img/pizza-logo.svg';
 import Search from './Search';
 
 function Header() {
+  const { items, totalPrice } = useSelector((state) => state.cart);
+
+  const totalCount = items.reduce((sum, item) => sum + item.count, 0);
+
   return (
     <div className="header">
       <div className="container">
@@ -22,7 +27,7 @@ function Header() {
         </div>
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
-            <span>520 ₽</span>
+            <span>{totalPrice} ₽</span>
             <div className="button__delimiter"></div>
             <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
@@ -47,7 +52,7 @@ function Header() {
                 strokeLinejoin="round"
               />
             </svg>
-            <span>3</span>
+            <span>{totalCount}</span>
           </Link>
         </div>
       </div>
